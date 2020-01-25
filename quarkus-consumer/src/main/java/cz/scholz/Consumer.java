8dd00ca8160d2288fa80eeddc87d34801a4760c4
Jakub Scholz <www@scholzj.com>
2020-01-25 15:31:21 +0100

    Remove REST

--- quarkus-consumer/src/main/java/cz/scholz/Consumer.java
+++ quarkus-consumer/src/main/java/cz/scholz/Consumer.java
@@ -1,24 +1,20 @@
 package cz.scholz;
 
-import io.smallrye.reactive.messaging.annotations.Broadcast;
 import io.smallrye.reactive.messaging.kafka.KafkaMessage;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.concurrent.CompletionStage;
 
 @ApplicationScoped
 public class Consumer {
     private static final Logger LOG = LoggerFactory.getLogger(Consumer.class.getName());
 
     @Incoming("consumed")
-    @Outgoing("new-times")
-    @Broadcast
-    public MyTime process(KafkaMessage<String, MyTime> msg) {
+    public CompletionStage<Void> consume(KafkaMessage<String, MyTime> msg) {
         LOG.info("Received message (topic: {}, partition: {}) with key {}: {}", msg.getTopic(), msg.getPartition(), msg.getKey(), msg.getPayload());
-        msg.ack();
-        return msg.getPayload();
+        return msg.ack();
     }
 }
--- quarkus-producer/src/main/java/cz/scholz/Producer.java
+++ quarkus-producer/src/main/java/cz/scholz/Producer.java
@@ -1,16 +1,19 @@
 package cz.scholz;
 
+import io.reactivex.Flowable;
 import io.smallrye.reactive.messaging.kafka.KafkaMessage;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 @ApplicationScoped
 public class Producer {
-    @Incoming("time-from-post")
     @Outgoing("produced")
-    public KafkaMessage<String, MyTime> generateMessages(MyTime time) {
-        return KafkaMessage.of("key", time);
+    public Flowable<KafkaMessage<String, MyTime>> generateMessages() {
+        return Flowable.interval(1, TimeUnit.SECONDS)
+                .map(tick -> KafkaMessage.of("key", MyTime.create(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime()))));
     }
 }
